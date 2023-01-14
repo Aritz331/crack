3de000af640bbb913d661c1fb15622a6ce09bb34
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-01-14 22:36:02 +0100

    Update install.bat

--- vegas/20/install.bat
+++ vegas/20/install.bat
@@ -19,7 +19,9 @@ call :ln
 call :vbmesg
 wscript //nologo %temp%\331\mesg.vbs "Please install VEGAS Pro normally (wait for the setup to appear) and wait for another message box to appear, then the program will be ready to be used.\n\nPor favor, instale VEGAS Pro con normalidad (espere a que el instalador se inicie) y espere a que aparezca otro mensaje, entonces el programa estará listo para ser usado." "VEGAS Pro %vegasver% Crack | by aritz331_" 0 64
 start /wait setup.exe
+ren vegas%vegasver%0.exe _vegas%vegasver%0.exe
 for /f "tokens=* delims=" %%i in ('dir "\vegas%vegasver%0.exe" /s /b') do (set "pathvegas=%%~dpi")
+ren _vegas%vegasver%0.exe vegas%vegasver%0.exe
 echo f|xcopy /y vegas%vegasver%0.exe "%pathvegas%"
 pause
 wscript //nologo %temp%\331\mesg.vbs "VEGAS Pro is ready.\n\nVEGAS Pro está listo." "VEGAS Pro %vegasver% Crack | by aritz331_" 0 64
